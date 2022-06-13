462995ab54d119667cd4948e9c967a9048d8d3b7
cryptoginger <keithlutes@gmail.com>
2022-06-12 20:35:22 -0600

    create Contact interface in new branch

--- src/interfaces/Contact.ts
+++ src/interfaces/Contact.ts
@@ -16,7 +16,6 @@ export interface ContactName {
 export interface Email {
 // email validation logic?
 //email = new RexExp (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-)
   email: string;
 }
 
